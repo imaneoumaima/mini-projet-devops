63e05bb21b5ab8e1b43a4be37aa73573e4ee296a
imaneoumaima <167689923+imaneoumaima@users.noreply.github.com>
2026-01-09 03:29:35 +0100

    Update app.py

--- services/grades/app.py
+++ services/grades/app.py
@@ -1,1 +1,21 @@
-print('grades service running')
+import logging
+from flask import Flask
+
+app = Flask(__name__)
+
+logging.basicConfig(
+    filename='/var/log/grades.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
+
+@app.route('/grades')
+def get_grades():
+    print("Grades service called")
+    logging.info("Grades service called")
+    return {"grades": [15, 12, 18]}
+
+if __name__ == '__main__':
+    print("grades service running")
+    logging.info("Grades service running")
+    app.run(host='0.0.0.0', port=5004)
